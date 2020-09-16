54fe1ea9676c63e7643486ce964c4b1dc2b96a90
sammy9867 <samuelmenezes15@gmail.com>
2020-09-16 18:50:01 +0200

    Updates

---
+++ precache-manifest.cbc36cbcedc7fa9067a8f459898f0dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0a980dfab42f35b3876d39ae73e23e",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "f56a20fc1bbceca53fb7",
+    "url": "/image-search/static/css/main.a991b641.chunk.css"
+  },
+  {
+    "revision": "efc53d84dc39cd083eca",
+    "url": "/image-search/static/js/2.37ec4d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search/static/js/2.37ec4d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56a20fc1bbceca53fb7",
+    "url": "/image-search/static/js/main.4c406b83.chunk.js"
+  },
+  {
+    "revision": "2f6c0911a5dc176631a9",
+    "url": "/image-search/static/js/runtime-main.ccae46a8.js"
+  }
+]);
